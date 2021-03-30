5de14ab4abe2eb7517525100e29807766e69e6c1
Martin Pashov <80414148+Martin-Pashov@users.noreply.github.com>
2021-03-30 22:37:14 +0300

    Add project files.

---
+++ Palindrome Extractor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Palindrome_Extractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter text wit palindromes (qnnq, aa, masam and so on...): ");
+            string input = Console.ReadLine();
+
+            string pattern = @"(.)(?:(.)(?:(.)\3?)?\2)?\1";
+            Regex regEx = new Regex(pattern);
+
+            MatchCollection matches = regEx.Matches(input);
+
+            foreach (Match item in matches)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
